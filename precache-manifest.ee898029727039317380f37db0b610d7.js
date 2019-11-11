4669492b3ba741c8798613bdadeb01f2d5edb5ca
natt <mac@natts-MacBook-Pro.local>
2019-11-11 23:56:03 +0700

    Updates

---
+++ precache-manifest.ee898029727039317380f37db0b610d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d2a3e4e7c63b87e499d39bd2bd39b4",
+    "url": "/nattoAS/bc-generate/index.html"
+  },
+  {
+    "revision": "b139147e45acd8ce824e",
+    "url": "/nattoAS/bc-generate/static/js/2.b2aa2bb2.chunk.js"
+  },
+  {
+    "revision": "eb97782a0c7c6e66578f",
+    "url": "/nattoAS/bc-generate/static/js/main.ba8c69c9.chunk.js"
+  },
+  {
+    "revision": "1df62006a9b5d8795652",
+    "url": "/nattoAS/bc-generate/static/js/runtime-main.6e11689b.js"
+  }
+]);
